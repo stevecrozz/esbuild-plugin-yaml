16de6f6318bfe1e2995b7c91b86e29c9c02590d8
Justin Casso <justincasso1@gmail.com>
2023-03-23 22:49:50 -0400

    Add caching for unchanged files

--- index.js
+++ index.js
@@ -4,12 +4,19 @@ const { readFile } = require("fs/promises");
 const yamlPlugin = () => ({
   name: "yaml",
   setup(build) {
+    const cache = new Map();
+
     build.onLoad({ filter: /.\.(yml|yaml)$/ }, async (args) => {
       const source = await readFile(args.path, "utf8");
-      const parsed = yaml.parse(source);
+      const cacheData = cache.get(args.path)
+
+      if (!cacheData || cacheData.source !== source) {
+        const parsed = yaml.parse(source);
+        cache.set(args.path, { source, parsed })
+      }
 
       return {
-        contents: JSON.stringify(parsed),
+        contents: JSON.stringify(cache.get(args.path).parsed),
         loader: "json",
       };
     });
